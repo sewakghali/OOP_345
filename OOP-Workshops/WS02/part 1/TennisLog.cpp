bf17c61114082058fb10714b3c84f6ce68ec10ba
Sewak Singh  Gill <74009096+sewakghali@users.noreply.github.com>
2022-09-25 19:44:10 -0400

    commit v1.2

--- OOP-Workshops/WS02/part 1/TennisLog.cpp
+++ OOP-Workshops/WS02/part 1/TennisLog.cpp
@@ -1,74 +1,109 @@
 #include<iomanip>
 #include<fstream>
+#include<cstring>
 #include "TennisLog.h"
 
 using namespace std;
 namespace sdds {
-   TennisMatch& operator =(TennisMatch& oldObj) {
-      matchId = oldObj.matchId;
-      tournamentID = oldObj.tournamentID;
-      tournamentName = oldObj.tournamentName;
-      winner = oldObj.winner;
-      loser = oldObj.loser;
+   TennisMatch::TennisMatch() {
+      matchId = 0;
+      tournamentID = "\0";
+      tournamentName = "\0";
+      winner = "\0";
+      loser = "\0";
+   }
+   TennisMatch::TennisMatch(const char* tID, const char* tName, int mId, const char* mWinner, const char* mLoser) {
+      this->matchId = mId;
+      this->tournamentID = string(tID);
+      this->tournamentName = string(tName);
+      this->winner = string(mWinner);
+      this->loser = string(mLoser);
+   }
+
+   inline TennisMatch& TennisMatch::operator=(const TennisMatch& oldObj) {
+      this->matchId = oldObj.matchId;
+      this->tournamentID = oldObj.tournamentID;
+      this->tournamentName = oldObj.tournamentName;
+      this->winner = oldObj.winner;
+      this->loser = oldObj.loser;
 
       return *this;
    }
 
+   /*TennisMatch::operator bool()const {
+      if(this.)
+   }*/
+
    ostream& operator << (ostream& os, TennisMatch& tm) {
-      if (tm.matchId == 0) {
-         os << "Empty Match" << endl;
-      }
-      else {
+      //std::cout << "extraction overload begins" << std::endl;
+      TennisMatch* ptr = &tm;
+      if (ptr) {
+         //os << "adress = " << &ptr << endl;
          os << "Tourney ID : " << tm.tournamentID << endl;
          os << "Match ID : " << tm.matchId << endl;
          os << "Tourney : " << tm.tournamentName << endl;
          os << "Winner : " << tm.winner << endl;
          os << "Loser : " << tm.loser << endl;
       }
+      else {
+         os << "Empty Match";
+      }
+      //std::cout << "extraction overload ends" << std::endl;
+
       return os;
    }
 
    TennisLog::TennisLog() {
-      TennisMatch* match_arr{};
+      TennisMatch* match_arr{ nullptr };
+      matchNum = 0;
    }
 
    TennisLog::TennisLog(const char* fname) {
       ifstream infile(fname);
       int count = 0;
       string temp;
-      while (!infile.EOF()) {
+      while (!infile.eof()) {
          getline(infile, temp);
          count++;
       }
+      infile.clear();
+      infile.seekg(0);
+
+      cout << "adding recs begins" << endl;
 
       match_arr = new TennisMatch[count];
-      for (int i = 0; i < count; i++) {
-         infile.get(match_arr[i].tournamentID);
+      getline(infile, temp);
+      for (int i = 0; i < count - 1; i++) {
+         getline(infile, match_arr[i].tournamentID, ',');
+         getline(infile, match_arr[i].tournamentName, ',');
+         infile >> match_arr[i].matchId;
          infile.ignore(1, ',');
-         infile.get(match_arr[i].tournamentName);
-         infile.ignore(1, ',');
-         infile >> match_arr[i].match;
-         infile.ignore(1, ',');
-         infile.get(match_arr[i].winner);
-         infile.ignore(1, ',');
-         infile.get(match_arr[i].loser);
-         infile.ignore(1, ',');
+         getline(infile, match_arr[i].winner, ',');
+         getline(infile, match_arr[i].loser);
+         //infile.ignore(1, '\n');
       }
+
+      cout << "adding recs ends" << endl;
 
       matchNum = count;
    }
 
    void TennisLog::addMatch(TennisMatch& matchObj) {
       TennisMatch* tempObj{};
-      tempObj = new TennisMatch[matchNum];
+      if (matchNum > 0) {
+         tempObj = new TennisMatch[matchNum];
+      }
+      else {
+
+      }
       for (int i = 0; i < matchNum; i++) {
-         tempObj[i] = match_arr[i];
+         tempObj[i] = this->match_arr[i];
       }
       delete[] match_arr;
       matchNum++;
       match_arr = new TennisMatch[matchNum];
 
-      for (int i = 0; i < matchNum-1; i++) {
+      for (int i = 0; i < matchNum - 1; i++) {
          match_arr[i] = tempObj[i];
       }
       match_arr[matchNum - 1] = matchObj;
@@ -76,19 +111,23 @@ namespace sdds {
       delete[] tempObj;
    }
 
-   TennisLog& TennisLog::findMatches(const char* playerName) {
+   TennisLog& TennisLog::findMatches(const char* player) {
       TennisLog tempObj;
-      for (int i < matchNum; i++) {
-         if (!strcmp(playerName, match_arr[i].loser) || !strcmp(playerName, match_arr[i].winner) {
+      string playerName = string(player);
+      for (int i = 0; i < matchNum; i++) {
+         if (playerName == match_arr[i].loser || playerName == match_arr[i].winner) {
             tempObj.addMatch(match_arr[i]);
          }
       }
       return tempObj;
    }
    TennisMatch& TennisLog::operator[](size_t arrIndex) {
+      if (arrIndex > matchNum) {
+         return match_arr[0];
+      }
       return match_arr[arrIndex];
    }
-   TennisLog& TennisLog::operator size_t() {
+   TennisLog::operator size_t() {
       return matchNum;
    }
 }
--- OOP-Workshops/WS02/part 1/TennisLog.h
+++ OOP-Workshops/WS02/part 1/TennisLog.h
@@ -4,16 +4,19 @@
 #include<string>
 
 namespace sdds {
-   typedef struct{
+   typedef struct TennisMatch{
       int matchId{0};
       std::string tournamentID{};
       std::string tournamentName{};
       std::string winner{};
       std::string loser{};
-   } TennisMatch;
-
-   TennisMatch& operator=(TennisMatch& oldObj);
+      TennisMatch();
+      TennisMatch(const char* tID, const char* tName, int mId, const char* mWinner, const char* mLoser);
+      TennisMatch& operator=(const TennisMatch& oldObj);
+      operator bool() const;
+   }TennisMatch;
    std::ostream& operator << (std::ostream& os, TennisMatch& tm);
+
 
    class TennisLog{
       TennisMatch* match_arr{};
@@ -26,6 +29,6 @@ public:
    void addMatch(TennisMatch&);
    TennisLog& findMatches(const char*);
    TennisMatch& operator[](size_t);
-   TennisLog& operator size_t();
+   operator size_t();
    };
 }
