c7db9572e466cc0c301c1ececa89dcd4c37594ec
Sewak Singh Gill <sgill116@myseneca.ca>
2022-11-19 23:27:31 -0500

    final commit MS1

--- OOP-Project/MS1/Station.h
+++ OOP-Project/MS1/Station.h
@@ -1,3 +1,11 @@
+//-------------------------------------------
+// OOP345 Project- Milestone 1
+// Module: Station.h
+// Name: Sewak Singh Gill
+// Student Id: 159282219
+// Email: sgill116@myseneca.ca
+//-------------------------------------------
+
 #pragma once
 #include<iostream>
 namespace sdds {
@@ -8,15 +16,26 @@ namespace sdds {
       size_t sNextSrl;
       size_t sStockCnt;
 
-      size_t m_widthField;
-      int id_generator;
+      static size_t m_widthField;
+      static int id_generator;
 
    public:
+      //constructor
       Station(const std::string&);
+
+      //returns sItem
       const std::string& getItemName() const;
+
+      //returns sNextSrl and updates it
       size_t getNextSerialNumber();
+
+      //returns  sStockCnt
       size_t getQuantity() const;
+
+      //substracts 1 from sStockCnt and returns sStockCnt
       void updateQuantity();
+
+      //displays Station data
       void display(std::ostream& os, bool full) const;
    };
 }
--- OOP-Project/MS1/Utilities.cpp
+++ OOP-Project/MS1/Utilities.cpp
@@ -1,22 +1,88 @@
+//-------------------------------------------
+// OOP345 Project- Milestone 1
+// Module: Utilities.cpp
+// Name: Sewak Singh Gill
+// Student Id: 159282219
+// Email: sgill116@myseneca.ca
+//-------------------------------------------
+
 #include<string>
 #include"Utilities.h"
 using namespace std;
 namespace sdds {
+   char Utilities::m_delimiter;
+
    void Utilities::setFieldWidth(size_t newWidth) {
       m_widthField = newWidth;
       return;
    }
-   size_t Utilities::getFieldWidth() const {
-      return m_fieldWidth;
-   }
-   std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more) {
 
+   size_t Utilities::getFieldWidth() const {
+      return m_widthField;
    }
-   static void Utilities::setDelimiter(char newDelimiter) {
+
+   std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more) {
+      std::string tempStr = "";
+      static size_t newWidth = 0; //stores the biggest number for tempStr length.
+      size_t findIndex = 0;
+      more = false;
+
+      //if delimiter is found at next_pos, throw an error.
+      if (str.find(m_delimiter, next_pos) == next_pos) {
+         more = false;
+         newWidth = 0;
+         throw "   ERROR: No token.";
+      }
+      else{
+         //error handling for std::string find method in case there is nothing found.
+         if (str.find(m_delimiter, next_pos) >= str.length()) {
+            findIndex = str.length();
+         }
+         else {
+            findIndex = str.find(m_delimiter, next_pos);
+         }
+
+         tempStr = str.substr(next_pos,findIndex-next_pos+1);
+         
+         //adding current size of string to next_pos
+         next_pos += tempStr.length();
+
+         //if next_pos >= initial string's length, it sets more to false to avoid reading from the string.
+         if (next_pos >= str.length()) {
+            more = false;
+         }
+         else {
+            tempStr.erase(tempStr.find(m_delimiter));
+            more = true;
+         }
+         tempStr = trim(tempStr);
+
+         //updating the width to max value of tempStr's size.
+         if (newWidth < tempStr.size()) {
+            newWidth = tempStr.size();
+            m_widthField = newWidth;
+         }
+
+         //sets newWidth back to zero once all values are read.
+         if (more == false) {
+            newWidth = 0;
+         }
+      }
+      return tempStr;
+   }
+
+   void Utilities::setDelimiter(char newDelimiter) {
       m_delimiter = newDelimiter;
       return;
    }
-   static char Utilities::getDelimiter() {
+
+   char Utilities::getDelimiter() {
       return m_delimiter;
+   }
+
+   std::string trim(std::string& str) {
+      str.erase(0, str.find_first_not_of(' '));
+      str.erase(str.find_last_not_of(' ')+1);
+      return str;
    }
 }
